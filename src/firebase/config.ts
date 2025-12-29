e663c65bf48dc858007096a4211de90cfe642158
Clinton Giri <bamegirry@gmail.com>
2025-12-29 01:27:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-797616227-2d20f",
+  "appId": "1:847423499100:web:e3ca226535b03a17e7af99",
+  "apiKey": "AIzaSyB9sXf8CyipXt88JxRhMoct1EMH1k-r8dI",
+  "authDomain": "studio-797616227-2d20f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "847423499100"
+};
