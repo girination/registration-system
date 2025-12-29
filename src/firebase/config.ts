350745df452f9914ac007ffd917270435e6e8d82
Clinton Giri <bamegirry@gmail.com>
2025-12-29 09:07:15 +0000

    Add them for me

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-797616227-2d20f",
-  "appId": "1:847423499100:web:e3ca226535b03a17e7af99",
-  "apiKey": "AIzaSyB9sXf8CyipXt88JxRhMoct1EMH1k-r8dI",
-  "authDomain": "studio-797616227-2d20f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "847423499100"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
 };
